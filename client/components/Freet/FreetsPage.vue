0e6a16ef22bcd9f5ab073da9b14bdc74afa0e569
AdorifyRJJ <118649741+AdorifyRJJ@users.noreply.github.com>
2022-11-23 02:13:13 -0500

    music! (#3)
    Co-authored-by: onceRonald <goronaldxu@gmail.com>

--- client/components/Freet/FreetsPage.vue
+++ client/components/Freet/FreetsPage.vue
@@ -6,7 +6,6 @@
       <header>
         <h2>Welcome @{{ $store.state.username }}</h2>
       </header>
-      <CreateFreetForm />
     </section>
     <section v-else>
       <header>
@@ -19,6 +18,8 @@
         </h3>
       </article>
     </section>
+    <button @click="playMusic">Play Music</button>
+    <button @click="pauseMusic">Pause Music</button>
     <section>
       <header>
         <div class="left">
@@ -60,14 +61,92 @@ import GetFreetsForm from "@/components/Freet/GetFreetsForm.vue";
 export default {
   name: "FreetPage",
   components: { FreetComponent, GetFreetsForm, CreateFreetForm },
+  data() {
+    return {
+      player: undefined,
+      player_device_id: undefined,
+    };
+  },
   async beforeCreate() {
     if (this.$route.query.code) {
-      await fetch(`/api/spotify/getToken?code=${this.$route.query.code}`);
+      const token = await fetch(
+        `/api/spotify/getToken?code=${this.$route.query.code}`
+      );
+      const tokenjson = await token.json();
       window.history.pushState({}, document.title, "/");
       const myData = await fetch("/api/spotify/getMe");
       const ans = await myData.json();
       console.log(ans);
+
+      const script = document.createElement("script");
+      script.src = "https://sdk.scdn.co/spotify-player.js";
+      script.async = true;
+
+      document.body.appendChild(script);
+
+      window.onSpotifyWebPlaybackSDKReady = () => {
+        const player = new window.Spotify.Player({
+          name: "Web Playback SDK",
+          getOAuthToken: (cb) => {
+            cb(tokenjson.token);
+          },
+          volume: 0.5,
+        });
+        console.log(player);
+        this.player = player;
+
+        player.addListener("ready", async ({ device_id }) => {
+          console.log("Ready with Device ID", device_id);
+          const playback = await fetch(
+            `/api/spotify/transfer?deviceId=${device_id}`
+          );
+          const playbackRes = await playback.json();
+          console.log(playbackRes);
+          this.player_device_id = device_id;
+        });
+
+        player.addListener("not_ready", ({ device_id }) => {
+          console.log("Device ID has gone offline", device_id);
+        });
+
+        player.addListener("player_state_changed", (state) => {
+          if (!state) {
+            return;
+          }
+
+          setTrack(state.track_window.current_track);
+          setPaused(state.paused);
+
+          player.getCurrentState().then((state) => {
+            !state ? setActive(false) : setActive(true);
+          });
+        });
+
+        player.connect();
+      };
     }
+  },
+  methods: {
+    async playMusic() {
+      console.log("toggle music");
+      if (this.player_device_id) {
+        const playback = await fetch(
+          `/api/spotify/play?deviceId=${this.player_device_id}`
+        );
+        const playbackRes = await playback.json();
+        console.log(playbackRes);
+      }
+    },
+    async pauseMusic() {
+      console.log("toggle music");
+      if (this.player_device_id) {
+        const playback = await fetch(
+          `/api/spotify/pause?deviceId=${this.player_device_id}`
+        );
+        const playbackRes = await playback.json();
+        console.log(playbackRes);
+      }
+    },
   },
 };
 </script>
