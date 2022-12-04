979d5c561c0eaf25fc0ee7d7cf03bbdd765f0d25
onceRonald <goronaldxu@gmail.com>
2022-12-04 16:34:12 -0500

    vuetify

--- client/pages/HomePage.vue
+++ client/pages/HomePage.vue
@@ -1,6 +1,7 @@
 <template>
   <main>
     <h1>Home Page</h1>
+    <v-btn>VUETIFY</v-btn>
     <!-- <div>
       <button @click="playMusic">Play Music</button>
       <button @click="pauseMusic">Pause Music</button>
