a77d43ad0ebb5756b8e212a811fecd56d6358c08
AdorifyRJJ <118649741+AdorifyRJJ@users.noreply.github.com>
2022-12-12 01:28:52 -0500

    Leaderboard frontend (#28)
    * ckpt
    
    * fix type
    
    * stuffs
    
    Co-authored-by: onceRonald <goronaldxu@gmail.com>

--- client/App.vue
+++ client/App.vue
@@ -10,7 +10,6 @@
 <script>
 import NavBar from "@/components/common/NavBar.vue";
 import SpotifyWebApi from "spotify-web-api-node";
-
 export default {
     name: "App",
     components: { NavBar },
@@ -30,21 +29,15 @@ export default {
             this.$store.commit("setExpiryTime", allDataJson.expiryTime);
             this.$store.commit("scheduleRefresh");
             this.$store.commit("refreshLikedPlaylists");
-
             const spotifyApi = new SpotifyWebApi({
                 clientId: process.env.ID,
             });
-
-      await spotifyApi.setAccessToken(allDataJson.accessToken);
-
+            spotifyApi.setAccessToken(allDataJson.accessToken);
             this.$store.commit("setSpotifyApi", spotifyApi);
-
             const script = document.createElement("script");
-
             script.src = "https://sdk.scdn.co/spotify-player.js";
             script.async = true;
             document.body.appendChild(script);
-
             window.onSpotifyWebPlaybackSDKReady = async () => {
                 const player = await new window.Spotify.Player({
                     name: "Web Playback SDK",
@@ -53,7 +46,6 @@ export default {
                     },
                     volume: 0.5,
                 });
-
                 player.addListener("ready", async ({ device_id }) => {
                     await fetch(`/api/spotify/transfer?deviceId=${device_id}`);
                     await fetch(`/api/spotify/pause?deviceId=${device_id}`);
@@ -62,16 +54,14 @@ export default {
                     this.$store.commit("setConnected", true);
                     console.log("Ready with Device ID", device_id);
                 });
-
                 player.addListener("not_ready", ({ device_id }) => {
                     console.log("Device ID has gone offline", device_id);
                     this.$store.commit("setConnected", false);
                 });
-
                 player.connect();
             };
-            if (this.$router.history.current.name !== "Home")
-                this.$router.push({ name: "Home" });
+            //if (this.$router.history.current.name !== "Home")
+            //    this.$router.push({ name: "Home" });
         } else {
             await fetch(`/api/spotify/logout`);
             this.$store.commit("resetStore");
@@ -81,7 +71,6 @@ export default {
     },
 };
 </script>
-
 <style>
 /* @font-face {
     font-family: "AdorifyF";
@@ -89,45 +78,38 @@ export default {
     font-weight: 100;
     font-style: normal;
 } */
-
 @font-face {
     font-family: "AdorifyF";
     src: url("./public/fonts/AdorifyF/AvenirNextLTPro-Regular.otf");
     font-weight: normal;
     font-style: normal;
 }
-
 @font-face {
     font-family: "AdorifyF";
     src: url("./public/fonts/AdorifyF/AvenirNextLTPro-Medium.otf");
     font-weight: medium;
     font-style: normal;
 }
-
 @font-face {
     font-family: "AdorifyF";
     src: url("./public/fonts/AdorifyF/AvenirNextLTPro-Bold.otf");
     font-weight: bold;
     font-style: normal;
 }
-
 * {
     box-sizing: border-box;
     font-family: "AdorifyF";
 }
-
 #app {
     color: white;
     background: #1f1b2e;
     /* width: 100vw; */
     height: 100vh;
 }
-
 ::-webkit-scrollbar {
     width: 0; /* Remove scrollbar space */
     background: transparent; /* Optional: just make scrollbar invisible */
 }
-
 body {
     height: 100vh;
     flex-direction: column;
@@ -136,23 +118,19 @@ body {
     margin: 0;
     font-size: 1.2em;
 }
-
 main {
     padding: 0 5em 5em;
 }
-
 .wh12b {
     color: #ffffff;
     font-size: 12px;
     font-weight: bold;
 }
-
 .wh16b {
     color: #ffffff;
     font-size: 16px;
     font-weight: bold;
 }
-
 .wh20b {
     color: #ffffff;
     font-size: 20px;
@@ -163,68 +141,56 @@ main {
     font-size: 30px;
     font-weight: bold;
 }
-
 .wh40b {
     color: #ffffff;
     font-size: 40px;
     font-weight: bold;
 }
-
 .wh50b {
     color: #ffffff;
     font-size: 50px;
     font-weight: bold;
 }
-
 .wh100b {
     color: #ffffff;
     font-size: 100px;
     font-weight: bold;
 }
-
 .wh20n {
     color: #ffffff;
     font-size: 20px;
     font-weight: normal;
 }
-
 .wh40n {
     color: #ffffff;
     font-size: 40px;
     font-weight: normal;
 }
-
 .gr12 {
     color: #a9a9a9;
     font-size: 12px;
 }
-
 .gr16 {
     color: #a9a9a9;
     font-size: 16px;
 }
-
 .gr20 {
     color: #a9a9a9;
     font-size: 20px;
 }
-
 .gr24 {
     color: #a9a9a9;
     font-size: 24px;
 }
-
 .gr30 {
     color: #a9a9a9;
     font-size: 30px;
 }
-
 .center {
     display: flex;
     flex-direction: column;
     align-items: center;
 }
-
 .button {
     color: #ffffff;
     background-color: #6c4eb3;
@@ -233,11 +199,9 @@ main {
     border-width: 0;
     padding: 0px 30px;
 }
-
 .button:hover {
     filter: brightness(80%);
 }
-
 .btn-group-button {
     color: #ffffff;
     background-color: #373544;
@@ -249,7 +213,6 @@ main {
     margin: 18px 0;
     display: flex;
 }
-
 .btn-group > .btn-group-button {
     border-right: 2px solid #6c4eb3;
 }
@@ -257,17 +220,14 @@ main {
     border-radius: 0 20px 20px 0;
     border-right: 0;
 }
-
 .btn-group > *:first-child {
     border-radius: 20px 0 0 20px;
 }
-
 .truncate1line {
     overflow: hidden;
     white-space: nowrap;
     text-overflow: ellipsis;
 }
-
 .truncate2lines {
     overflow: hidden;
     text-overflow: ellipsis;
@@ -275,11 +235,9 @@ main {
     -webkit-line-clamp: 2;
     -webkit-box-orient: vertical;
 }
-
 a:link {
     text-decoration: none;
 }
-
 a:visited {
     text-decoration: none;
 }
