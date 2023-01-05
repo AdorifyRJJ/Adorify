5642d1bda26144983a649b62a4cf7e0911f9b310
Jerry Li <jerryyli12@gmail.com>
2023-01-04 23:08:33 -0500

    rly bad at code

--- server/adorifysession/util.ts
+++ server/adorifysession/util.ts
@@ -6,20 +6,44 @@ import { Playlist } from "../playlist/model";
 // }
 
 export const generateLast28Days = (tzoffset: number): Array<Date> => {
-    const today = new Date();
-    const dates = [today]; // this return is never used
-    console.log(today)
+    // const today = new Date();
+    // const dates = [today]; // this return is never used
+
+    // const localTime = new Date(today.getTime() - tzoffset*60*1000);
+    // console.log(today.getDate(), localTime.getDate())
+
+    // const dayoffset = today.getDate() === localTime.getDate() ? 0 : 1;
+
+    // for (let i = 0; i < 29; i++) {
+    //     const date = new Date();
+    //     date.setDate(date.getDate() - i - dayoffset)
+    //     date.setHours(0);
+    //     date.setMinutes(0);
+    //     date.setSeconds(0);
+    //     date.setMilliseconds(0);
+    //     date.setTime(date.getTime() + tzoffset*60*1000)
+    //     dates.push(date)
+    // }
+    // return dates;
+
+    const now = new Date();
+    const dates = [now];
+
+    const localTime = new Date(now.getTime() - tzoffset*60*1000);
+    const startOfDayLocal = new Date(localTime.getTime());
+    startOfDayLocal.setUTCHours(0, 0, 0, 0);
+
+    console.log(localTime, startOfDayLocal)
+    const elapsedTimeLocal = localTime.getTime() - startOfDayLocal.getTime();
+    console.log(elapsedTimeLocal / 1000 / 60);
+
     for (let i = 0; i < 29; i++) {
-        const date = new Date();
-        date.setDate(date.getDate() - i);
-        date.setHours(0);
-        date.setMinutes(0);
-        date.setSeconds(0);
-        date.setMilliseconds(0);
-        date.setTime(date.getTime() + tzoffset*60*1000)
-        if (i === 0) console.log("yo", date)
-        dates.push(date)
+        const begOfDay = new Date(now.getTime());
+        begOfDay.setDate(begOfDay.getDate() - i);
+        console.log(new Date(begOfDay.getTime() - elapsedTimeLocal))
+        dates.push(new Date(begOfDay.getTime() - elapsedTimeLocal));
     }
+
     return dates;
 }
 
