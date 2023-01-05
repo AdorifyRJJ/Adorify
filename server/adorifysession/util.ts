599401a32ea48381932b991e7bb6649cc534ab2f
Jerry Li <jerryyli12@gmail.com>
2023-01-04 22:20:49 -0500

    bad at code

--- server/adorifysession/util.ts
+++ server/adorifysession/util.ts
@@ -17,7 +17,7 @@ export const generateLast28Days = (tzoffset: number): Array<Date> => {
         date.setSeconds(0);
         date.setMilliseconds(0);
         date.setTime(date.getTime() + tzoffset*60*1000)
-        if (0 === 1) console.log("yo", date)
+        if (i === 0) console.log("yo", date)
         dates.push(date)
     }
     return dates;
